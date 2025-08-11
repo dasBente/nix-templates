909a0c7d0c4d62daa90a398d40b269b7e00900df
dasBente <dasbente@gmail.com>
2025-08-11 19:43:03 +0000

    add basic clojure template

---
+++ templates/clojure/flake.nix
@@ -0,0 +1,34 @@
+{
+  description = "Basic setup for Clojure development";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, utils }: 
+  utils.lib.eachDefaultSystem (system:
+    let
+      javaVersion = 23;
+      jdk = "jdk${toString javaVersion}";
+      pkgs = import nixpkgs {
+        inherit system;
+        overlays = [
+          (self: super: {
+            jdk = super."${jdk}";
+            clojure = super.clojure.override {
+              jdk = super."${jdk}";
+            };
+            # lein = super.clojure.override { jdk = super."${jdk}"; };
+          })
+        ];
+      };
+    in {
+      devShells.default = pkgs.mkShell {
+        packages = with pkgs; [
+          clojure
+        ];
+      };
+    }
+  );
+}
